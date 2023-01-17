737cc5006ff450ff0016fc50a91e15c9db8d71c6
Shashilo Alexey <a.shashilo1998@gmail.com>
2023-01-17 19:58:42 +0700

    editing methods clear, save, get, delete, getAll and size

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,58 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.getUuid().equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                storage[i] = storage[i + 1];
+                break;
+            }
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] r = new Resume[size()];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                r[i] = storage[i];
+            }
+        }
+        return r;
     }
 
     int size() {
-        return 0;
+        int size = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            size += 1;
+        }
+        return size;
     }
 }
