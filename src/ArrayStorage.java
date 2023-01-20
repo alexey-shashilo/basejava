1f5407638c36956f406254a4cc6f755a6cd3783d
Shashilo Alexey <a.shashilo1998@gmail.com>
2023-01-20 23:14:23 +0700

    editing methods save, delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,8 +4,8 @@ import static java.lang.System.arraycopy;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int countResume = 0;
+    private Resume[] storage = new Resume[10000];
+    private int countResume = 0;
 
     void clear() {
         for (int i = 0; i < countResume; i++) {
